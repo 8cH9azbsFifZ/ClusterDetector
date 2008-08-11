9c133a9541101fbefe89d7323fe2dad16363c31a
Gerolf Ziegenhain <gerolf@ziegenhain.com>
2008-08-11 08:55:11 +0200

    .

--- Cluster.py
+++ Cluster.py
@@ -7,7 +7,21 @@ filename = sys.argv[1]
 h1 = tables.openFile(filename)
 particles = h1.root.Particles.All
 
-p = [particles.read()["x"], particles.read()["y"], particles.read()["z"]]
-natoms = h1.root.Properties.natoms
+[x, y, z] = [particles.read()["x"], particles.read()["y"], particles.read()["z"]]
+natoms = numpy.size (x)
 print natoms
-List = numpy.array((natoms,1))
+
+def InitList():
+   List = numpy.array((natoms,1))
+   for i in range(0,natoms):
+      List[i] = i
+
+def Clustere():
+   for i in range(0,natoms-1):
+      if i != List[i]:
+         continue
+      j = i
+      Xj = x[j]
+      Yj = y[j]
+      Zj = z[j]
+
