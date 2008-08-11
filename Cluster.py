f37f7d6c43092324d7d34f7cf5826727a1e1afcc
Gerolf Ziegenhain <gerolf@ziegenhain.com>
2008-08-11 09:04:09 +0200

    .

--- Cluster.py
+++ Cluster.py
@@ -11,17 +11,42 @@ particles = h1.root.Particles.All
 natoms = numpy.size (x)
 print natoms
 
-def InitList():
-   List = numpy.array((natoms,1))
-   for i in range(0,natoms):
-      List[i] = i
+def SqDist(a,b):
+   return (a-b)**2
 
-def Clustere():
-   for i in range(0,natoms-1):
-      if i != List[i]:
-         continue
-      j = i
-      Xj = x[j]
-      Yj = y[j]
-      Zj = z[j]
+class Cluster:
+   def InitList(self):
+      print "Init list"
+      self.List = numpy.array ((self.natoms,1))
+      for i in range(0,natoms):
+         self.List[i] = i
+
+
+   def Clustere():
+      print "Clustere"
+      for i in range(0, self.natoms-1):
+         if i != self.List[i]:
+            continue
+
+         j = i
+         Xj = self.x[j]
+         Yj = self.y[j]
+         Zj = self.z[j]
+
+         while True:
+            for k in  range(i+1, self.natoms):
+               Lk = self.List[k]
+               if k == Lk:
+                  RSq = SqDist(Xj, self.x[k]) + SqDist(Yj, self.y[k]) + SqDist(Zj, self.z[k])
+                  if RSq <= self.RcClusterSq:
+                     self.List[k] = self.List[j]
+                     self.List[j] = Lk
+            
+            j = self.List[j]
+            Xj = self.x[j]
+            Yj = self.y[j]
+            Zj = self.z[j]
+            
+            if i == j:
+               break
 
